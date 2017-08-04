41de46f959b7478672d697dd5427f614db4c1ad7
John Carpenter <jcarpent@gmail.com>
2017-08-04 10:17:29 -0600

    just why?

--- game.js
+++ game.js
@@ -1,11 +1,10 @@
+
 // Mimic Me!
 // Fun game where you need to express emojis being displayed
-
 // --- Affectiva setup ---
-
 // The affdex SDK Needs to create video and canvas elements in the DOM
 var divRoot = $("#camera")[0];  // div node where we want to add these elements
-var width = 640, height = 480;  // camera image size
+var width = 320, height = 240;  // camera image size
 var faceMode = affdex.FaceDetectorMode.LARGE_FACES;  // face mode parameter
 
 // Initialize an Affectiva CameraDetector object
@@ -23,11 +22,12 @@ var gameState = {
   currentEmoji: null,
   reset: function(){
     this.score = 0;
+    setScore(this.score,false);
     this.currentEmoji = null;
   },
   nextEmoji: function(){
-    this.currentEmoji = emojis[Math.floor(Math.random() * emojis.length)];
-    setTargetEmoji(this.currentEmoji);
+    this.currentEmoji = getRandomEmoji();
+    showNextEmoji(this.currentEmoji);
   },
   checkEmoji: function(againstEmoji){
     if(this.currentEmoji == toUnicode(againstEmoji)){
@@ -41,9 +41,8 @@ var gameState = {
 // Unicode values for all emojis Affectiva can detect
 var emojis = [ 128528, 9786, 128515, 128524, 128527, 128521, 128535, 128539, 128540, 128542, 128545, 128563, 128561 ];
 
-// Update target emoji being displayed by supplying a unicode value
-function setTargetEmoji(code) {
-  $("#target").html("&#" + code + ";");
+function getRandomEmoji(){
+  return emojis[Math.floor(Math.random() * emojis.length)];
 }
 
 // Convert a special character to its unicode value (can be 1 or 2 units long)
@@ -53,14 +52,11 @@ function toUnicode(c) {
   return ((((c.charCodeAt(0) - 0xD800) * 0x400) + (c.charCodeAt(1) - 0xDC00) + 0x10000));
 }
 
-// Update score being displayed
-function setScore(score) {
-  $("#score").html("Score: " + score);
-}
+
 
 // Display log messages and tracking results
-function log(node_name, msg) {
-  $(node_name).append("<span>" + msg + "</span><br />")
+function log(msg,level='all') {
+    console.log(msg);
 }
 
 // --- Callback functions ---
@@ -68,10 +64,9 @@ function log(node_name, msg) {
 // Start button
 function onStart() {
   if (detector && !detector.isRunning) {
-    $("#logs").html("");  // clear out previous log
     detector.start();  // start detector
   }
-  log('#logs', "Start button pressed");
+  log("Start Game pressed");
 
   // reset score
   gameState.reset();
@@ -80,56 +75,39 @@ function onStart() {
 
 // Stop button
 function onStop() {
-  log('#logs', "Stop button pressed");
+  log("Stop game pressed");
   if (detector && detector.isRunning) {
     detector.removeEventListener();
     detector.stop();  // stop detector
   }
-
   // reset score
   gameState.reset();
-};
-
-// Reset button
-function onReset() {
-  log('#logs', "Reset button pressed");
-  if (detector && detector.isRunning) {
-    detector.reset();
-  }
-  $('#results').html("");  // clear out results
-  $("#logs").html("");  // clear out previous log
-
-  // reset score
-  gameState.reset();
-  gameState.nextEmoji();
 };
 
 // Add a callback to notify when camera access is allowed
 detector.addEventListener("onWebcamConnectSuccess", function() {
-  log('#logs', "Webcam access allowed");
+  log("Webcam access allowed");
 });
 
 // Add a callback to notify when camera access is denied
 detector.addEventListener("onWebcamConnectFailure", function() {
-  log('#logs', "webcam denied");
-  console.log("Webcam access denied");
+  log("webcam denied");
+  // @todo end
 });
 
 // Add a callback to notify when detector is stopped
 detector.addEventListener("onStopSuccess", function() {
-  log('#logs', "The detector reports stopped");
-  $("#results").html("");
+  log("The detector reports stopped");
 });
 
 // Add a callback to notify when the detector is initialized and ready for running
 detector.addEventListener("onInitializeSuccess", function() {
-  log('#logs', "The detector reports initialized");
+  log("The detector reports initialized");
   //Display canvas instead of video feed because we want to draw the feature points on it
   $("#face_video_canvas").css("display", "block");
   $("#face_video").css("display", "none");
-
-  // TODO(optional): Call a function to initialize the game, if needed
-  // <your code here>
+  $("#loading").hide();
+  $("#game").show();
 });
 
 // Add a callback to receive the results from processing an image
@@ -140,26 +118,21 @@ detector.addEventListener("onImageResultsSuccess", function(faces, image, timest
   if (!canvas)
     return;
 
-  // Report how many faces were found
-  $('#results').html("");
-  log('#results', "Timestamp: " + timestamp.toFixed(2));
-  log('#results', "Number of faces found: " + faces.length);
   if (faces.length > 0) {
     // Report desired metrics
-    log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
-    log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
+    log("Appearance: " + JSON.stringify(faces[0].appearance));
+    log("Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
       return val.toFixed ? Number(val.toFixed(0)) : val;
     }));
-    log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
+    log("Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
       return val.toFixed ? Number(val.toFixed(0)) : val;
     }));
-    log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
+    log("Emoji: " + faces[0].emojis.dominantEmoji);
 
     // Call functions to draw feature points and dominant emoji (for the first face only)
     drawFeaturePoints(canvas, image, faces[0]);
     drawEmoji(canvas, image, faces[0]);
 
-    // TODO: Call your function to run the game (define it first!)
     if(faces[0].emojis != null && faces[0].emojis.dominantEmoji != null){
       gameState.checkEmoji(faces[0].emojis.dominantEmoji);
     }
@@ -188,31 +161,75 @@ function drawEmoji(canvas, img, face) {
 
   var emoji = face.emojis.dominantEmoji;
 
-  // Usually its the center
-  var featurePoint = face.featurePoints[13];
-
   if(emoji != 0){
     ctx.font = '48px serif';
-    ctx.fillText(emoji, featurePoint.x, featurePoint.y);
+    ctx.fillText(emoji, 10, 50);
   }
 }
 
-// TODO: Define any variables and functions to implement the Mimic Me! game mechanics
-
-// NOTE:
-// - Remember to call your update function from the "onImageResultsSuccess" event handler above
-// - You can use setTargetEmoji() and setScore() functions to update the respective elements
-// - You will have to pass in emojis as unicode values, e.g. setTargetEmoji(128578) for a simple smiley
-// - Unicode values for all emojis recognized by Affectiva are provided above in the list 'emojis'
-// - To check for a match, you can convert the dominant emoji to unicode using the toUnicode() function
-
-// Optional:
-// - Define an initialization/reset function, and call it from the "onInitializeSuccess" event handler above
-// - Define a game reset function (same as init?), and call it from the onReset() function above
-
-// <your code here>
 function drawPoint(x, y, canvas){
   canvas.beginPath();
   canvas.arc(x, y, 1, 0, 2 * Math.PI, true);
   canvas.stroke();
 }
+
+// Game
+var app = new PIXI.Application();
+app.renderer = PIXI.autoDetectRenderer(800, 600, { transparent: true });
+
+
+// Emoji Container
+var emojiContainer = new PIXI.Container();
+// Center on the screen
+emojiContainer.x = (app.renderer.width - emojiContainer.width) / 2;
+emojiContainer.y = (app.renderer.height - emojiContainer.height) / 2;
+app.stage.addChild(emojiContainer);
+
+var emojiTextStyle = new PIXI.TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 144,
+    fontWeight: 'bold',
+});
+function showNextEmoji(emoji){
+  var text = String.fromCodePoint(emoji);
+  var basicText = new PIXI.Text(text,emojiTextStyle);
+  basicText.anchor.set(0.5);
+  emojiContainer.addChild(basicText);
+}
+
+// Update score being displayed
+var snd = new Audio("audio/points.wav"); // buffers automatically when created
+
+function setScore(score) {
+  log("Setting Score");
+  snd.play();
+  $("#score_label").html("Score:"+score);
+}
+
+$(document).ready(function() {
+
+    onStart();
+
+    $("#loading").show();
+    $("#game").hide();
+
+  	// add the renderer view element to the DOM
+  	$("#gameContainer").append(app.view);
+
+  	app.ticker.add(function(delta){
+
+
+    });
+
+    $(window).keypress(function (e) {
+      if (e.keyCode === 0 || e.keyCode === 32) {
+        gameState.nextEmoji();
+      }else if (e.keyCode === 0 || e.keyCode === 109) {
+        var audio = $("audio")[0];
+        if (audio.paused) { audio.play(); } else { audio.pause(); }
+      }
+
+
+    });
+
+  });
